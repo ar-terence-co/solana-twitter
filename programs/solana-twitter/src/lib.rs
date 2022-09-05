902e01d879748d61ea342601a574ee0b115255d2
ar-terence-co <mrctrncco@gmail.com>
2022-09-05 12:48:15 +0800

    Add update and delete to contract

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("BUW39Wm8Q3cXfkbQ6aesRKARc3bKxUJL3vHWFMn6ntRz");
 pub mod solana_twitter {
     use super::*;
 
-    pub fn send_tweet(ctx: Context<SendTweet>, uuid: [u8; 16], topic: String, content: String) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String, unique_seed: [u8; 16]) -> Result<()> {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
@@ -19,61 +19,104 @@ pub mod solana_twitter {
             return err!(ErrorCode::ContentTooLong);
         }
 
-        tweet.uuid = uuid;
         tweet.author = *author.key;
-        tweet.timestamp = clock.unix_timestamp;
+        tweet.created_timestamp = clock.unix_timestamp;
+        tweet.updated_timestamp = tweet.created_timestamp;
         tweet.topic = topic;
         tweet.content = content;
+        tweet.unique_seed = unique_seed;
         tweet.bump = *ctx.bumps.get("tweet").unwrap();
 
+        Ok(())
+    }
+
+    pub fn update_tweet(ctx: Context<UpdateTweet>, topic: String, content: String) -> Result<()> {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let clock: Clock = Clock::get().unwrap();
+
+        if topic.chars().count() > 50 {
+            return err!(ErrorCode::TopicTooLong);
+        }
+
+        if content.chars().count() > 280 {
+            return err!(ErrorCode::ContentTooLong);
+        }
+
+        tweet.updated_timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
+        Ok(())
+    }
+
+    pub fn delete_tweet(_ctx: Context<DeleteTweet>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(uuid: [u8; 16], topic: String, content: String)]
+#[instruction(topic: String, content: String, unique_seed: [u8; 16])]
 pub struct SendTweet<'info> {
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(init, payer = author, space = Tweet::LEN, seeds = [b"tweet", author.key().as_ref(), uuid.as_ref()], bump)]
+    #[account(init, payer = author, space = Tweet::LEN, seeds = [b"tweet", author.key().as_ref(), unique_seed.as_ref()], bump)]
     pub tweet: Account<'info, Tweet>,
     pub system_program: Program<'info, System>,
 }
 
-// #[derive(Accounts)]
-// pub struct UpdateTweet<'info> {
-//     #[account(mut)]
-//     pub tweet: Account<'info, Tweet>,
-//     #[account(mut)]
-//     pub author: Signer<'info>
-// }
+#[derive(Accounts)]
+pub struct UpdateTweet<'info> {
+    #[account(mut)]
+    pub author: Signer<'info>,
+    #[account(
+        mut, 
+        seeds = [b"tweet", author.key().as_ref(), tweet.unique_seed.as_ref()], 
+        bump = tweet.bump,
+    )]
+    pub tweet: Account<'info, Tweet>,
+}
+
+#[derive(Accounts)]
+pub struct DeleteTweet<'info> {
+    #[account(mut)]
+    pub author: Signer<'info>,
+    #[account(
+        mut,
+        close = author,
+        seeds = [b"tweet", author.key().as_ref(), tweet.unique_seed.as_ref()], 
+        bump = tweet.bump,
+    )]
+    pub tweet: Account<'info, Tweet>,
+}
 
 #[account]
 pub struct Tweet {
-    pub uuid: [u8; 16],
     pub author: Pubkey,
-    pub timestamp: i64,
+    pub created_timestamp: i64,
+    pub updated_timestamp: i64,
     pub topic: String,
     pub content: String,
+    pub unique_seed: [u8; 16],
     pub bump: u8,
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
-const UUID_LENGTH: usize = 16;
 const PUBKEY_LENGTH: usize = 32;
 const TIMESTAMP_LENGTH: usize = 8;
 const STRING_PREFIX_LENGTH: usize = 4; // Stores the size of the string
 const MAX_TOPIC_LENGTH: usize = 50 * 4; // Max of 50 chars in UTF-8 encoding
 const MAX_CONTENT_LENGTH: usize = 280 * 4; // Max of 280 chars in UTF-8 encoding
+const UNIQUE_SEED_LENGTH: usize = 16;
 const BUMP_LENGTH: usize = 1;
 
 impl Tweet {
     const LEN: usize = DISCRIMINATOR_LENGTH 
-        + UUID_LENGTH
         + PUBKEY_LENGTH // Author
-        + TIMESTAMP_LENGTH // Timestamp
+        + TIMESTAMP_LENGTH // Created Timestamp
+        + TIMESTAMP_LENGTH // Updated Timestamp
         + STRING_PREFIX_LENGTH + MAX_TOPIC_LENGTH // Topic
         + STRING_PREFIX_LENGTH + MAX_CONTENT_LENGTH // Content
+        + UNIQUE_SEED_LENGTH
         + BUMP_LENGTH;
 }
 
--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -3,13 +3,20 @@ import { AnchorError, Program } from "@project-serum/anchor";
 import { SolanaTwitter } from "../target/types/solana_twitter";
 import * as assert from "assert";
 import * as bs58 from "bs58";
-import { v4 as uuidV4, parse as uuidParse } from "uuid";
+import { parse as uuidParse } from "uuid";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+  const uuids = [
+    "9944165c-fc9d-4047-897b-322e3a6973bb",
+    "fb9b1142-9f30-4a3b-b09e-c27808127e5f",
+    "59193c93-89f7-4fa0-b965-3ca64270fe5a",
+    "10bcd7ac-2729-4faa-b52b-75677aaceaf4",
+    "bac9a115-7139-48d2-8436-e001fb521a45",
+  ];
 
   const airdropToAccount = async (publicKey: anchor.web3.PublicKey, lamports: number) => {
     const airdropSig = await program.provider.connection.requestAirdrop(publicKey, lamports);
@@ -22,14 +29,14 @@ describe("solana-twitter", () => {
   }
 
   it("can send a new tweet", async () => {
-    const uuid = new anchor.BN(uuidParse(uuidV4()));
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[0]));
     const authorPublicKey = program.provider.publicKey;
     const [tweetPDA, bump] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uuid.toArrayLike(Buffer)], 
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
       program.programId,
     )
 
-    await program.methods.sendTweet(uuid.toArray(), "veganism", "Hummus, am I right?")
+    await program.methods.sendTweet("veganism", "Hummus, am I right?", uniqueSeed.toArray())
       .accounts({
         author: authorPublicKey,
         tweet: tweetPDA,
@@ -39,23 +46,24 @@ describe("solana-twitter", () => {
 
     const tweetAccount = await program.account.tweet.fetch(tweetPDA);
     
-    assert.deepEqual(tweetAccount.uuid, uuid.toArray());
     assert.equal(tweetAccount.author.toBase58(), authorPublicKey.toBase58());
     assert.equal(tweetAccount.topic, "veganism");
     assert.equal(tweetAccount.content, "Hummus, am I right?");
+    assert.deepEqual(tweetAccount.uniqueSeed, uniqueSeed.toArray());
     assert.equal(tweetAccount.bump, bump);
-    assert.ok(tweetAccount.timestamp);
+    assert.ok(tweetAccount.createdTimestamp);
+    assert.ok(tweetAccount.updatedTimestamp);
   });
 
   it("can send a new tweet without a topic", async () => {
-    const uuid = new anchor.BN(uuidParse(uuidV4()));    
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[1]));
     const authorPublicKey = program.provider.publicKey;
     const [tweetPDA, bump] = await anchor.web3.PublicKey.findProgramAddress(
-      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uuid.toArrayLike(Buffer)], 
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
       program.programId,
     )
 
-    await program.methods.sendTweet(uuid.toArray(), "", "gm")
+    await program.methods.sendTweet("", "gm", uniqueSeed.toArray())
       .accounts({
         author: authorPublicKey,
         tweet: tweetPDA,
@@ -65,113 +73,277 @@ describe("solana-twitter", () => {
 
     const tweetAccount = await program.account.tweet.fetch(tweetPDA);
     
-    assert.deepEqual(tweetAccount.uuid, uuid.toArray());
     assert.equal(tweetAccount.author.toBase58(), authorPublicKey.toBase58());
     assert.equal(tweetAccount.topic, "");
     assert.equal(tweetAccount.content, "gm");
+    assert.deepEqual(tweetAccount.uniqueSeed, uniqueSeed.toArray());
     assert.equal(tweetAccount.bump, bump);
-    assert.ok(tweetAccount.timestamp);
+    assert.ok(tweetAccount.createdTimestamp);
+    assert.ok(tweetAccount.updatedTimestamp);
   });
 
-  // it("can send a new tweet from a different user", async () => {
-  //   const otherUser = anchor.web3.Keypair.generate();
-  //   await airdropToAccount(otherUser.publicKey, 1000000000);
+  it("can send a new tweet from a different user", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    await airdropToAccount(otherUser.publicKey, 1000000000);
 
-  //   const tweet = anchor.web3.Keypair.generate();
-  //   await program.methods.sendTweet("veganism", "Yay! Tofu!")
-  //     .accounts({
-  //       tweet: tweet.publicKey,
-  //       author: otherUser.publicKey,
-  //       systemProgram: anchor.web3.SystemProgram.programId,
-  //     })
-  //     .signers([otherUser, tweet])
-  //     .rpc();
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[2]));
+    const authorPublicKey = otherUser.publicKey;
+    const [tweetPDA, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
 
-  //   const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    await program.methods.sendTweet("veganism", "Yay! Tofu!", uniqueSeed.toArray())
+      .accounts({
+        tweet: tweetPDA,
+        author: authorPublicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([otherUser])
+      .rpc();
+
+    const tweetAccount = await program.account.tweet.fetch(tweetPDA);
     
-  //   assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
-  //   assert.equal(tweetAccount.topic, "veganism");
-  //   assert.equal(tweetAccount.content, "Yay! Tofu!");
-  //   assert.ok(tweetAccount.timestamp);
-  // });
+    assert.equal(tweetAccount.author.toBase58(), otherUser.publicKey.toBase58());
+    assert.equal(tweetAccount.topic, "veganism");
+    assert.equal(tweetAccount.content, "Yay! Tofu!");
+    assert.deepEqual(tweetAccount.uniqueSeed, uniqueSeed.toArray());
+    assert.equal(tweetAccount.bump, bump);
+    assert.ok(tweetAccount.createdTimestamp);
+    assert.ok(tweetAccount.updatedTimestamp);
+  });
 
-  // it("can not provide a topic with more than 50 characters", async () => {
-  //   const tweet = anchor.web3.Keypair.generate();
-  //   const topicWith51Chars = 'x'.repeat(51);
+  it("can not provide a topic with more than 50 characters", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[3]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+    const topicWith51Chars = 'x'.repeat(51);
 
-  //   await assert.rejects(
-  //     program.methods.sendTweet(topicWith51Chars, "gm")
-  //       .accounts({
-  //         tweet: tweet.publicKey,
-  //         author: program.provider.publicKey,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       })
-  //       .signers([tweet])
-  //       .rpc(),
-  //     (err: AnchorError) => {
-  //       assert.equal(err.error.errorMessage, "The provided topic should be 50 characters long maximum.");
-  //       return true;
-  //     },
-  //     "The instruction should have failed with a 51-character topic.",
-  //   );
-  // });
+    await assert.rejects(
+      program.methods.sendTweet(topicWith51Chars, "gm", uniqueSeed.toArray())
+        .accounts({
+          tweet: tweetPDA,
+          author: authorPublicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "The provided topic should be 50 characters long maximum.");
+        return true;
+      },
+      "The instruction should have failed with a 51-character topic.",
+    );
+  });
 
-  // it("can not provide a content with more than 280 characters", async () => {
-  //   const tweet = anchor.web3.Keypair.generate();
-  //   const contentWith51Chars = 'x'.repeat(281);
+  it("can not provide a content with more than 280 characters", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[4]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+    const contentWith51Chars = 'x'.repeat(281);
 
-  //   await assert.rejects(
-  //     program.methods.sendTweet("veganism", contentWith51Chars)
-  //       .accounts({
-  //         tweet: tweet.publicKey,
-  //         author: program.provider.publicKey,
-  //         systemProgram: anchor.web3.SystemProgram.programId,
-  //       })
-  //       .signers([tweet])
-  //       .rpc(),
-  //     (err: AnchorError) => {
-  //       assert.equal(err.error.errorMessage, "The provided content should be 280 characters long maximum.");
-  //       return true;
-  //     },
-  //     "The instruction should have failed with a 281-character content.",
-  //   );
-  // });
+    await assert.rejects(
+      program.methods.sendTweet("veganism", contentWith51Chars, uniqueSeed.toArray())
+        .accounts({
+          tweet: tweetPDA,
+          author: program.provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "The provided content should be 280 characters long maximum.");
+        return true;
+      },
+      "The instruction should have failed with a 281-character content.",
+    );
+  });
 
-  // it("can fetch all tweets", async () => {
-  //   const tweetAccounts = await program.account.tweet.all();
-  //   assert.equal(tweetAccounts.length, 3);
-  // });
+  it("can update a tweet", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[1]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
 
-  // it("can filter tweets by author", async () => {
-  //   const authorPublicKey = program.provider.publicKey;
-  //   const tweetAccounts = await program.account.tweet.all([
-  //     {
-  //       memcmp: {
-  //         offset: 8, //Discriminator offset,
-  //         bytes: authorPublicKey.toBase58(),
-  //       }
-  //     }
-  //   ]);
+    await program.methods.updateTweet("sleep", "gn")
+      .accounts({
+        author: authorPublicKey,
+        tweet: tweetPDA,
+      })
+      .rpc();
 
-  //   assert.equal(tweetAccounts.length, 2);
-  //   assert.ok(tweetAccounts.every(tweetAccount => 
-  //     tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
-  //   ));
-  // });
+    const tweetAccount = await program.account.tweet.fetch(tweetPDA);
+    
+    assert.equal(tweetAccount.author.toBase58(), authorPublicKey.toBase58());
+    assert.equal(tweetAccount.topic, "sleep");
+    assert.equal(tweetAccount.content, "gn");
+    assert.deepEqual(tweetAccount.uniqueSeed, uniqueSeed.toArray());
+    assert.equal(tweetAccount.bump, bump);
+    assert.ok(tweetAccount.updatedTimestamp > tweetAccount.createdTimestamp);
+  });
 
-  // it("can filter tweets by topics", async () => {
-  //   const tweetAccounts = await program.account.tweet.all([
-  //     {
-  //       memcmp: {
-  //         offset: 8 + 32 + 8 + 4, // Discriminator + Author + Timestamp + Topic Length
-  //         bytes: bs58.encode(Buffer.from("veganism")),
-  //       }
-  //     }
-  //   ]);
+  it("can not update the tweet of a different user", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    await airdropToAccount(otherUser.publicKey, 1000000000);
 
-  //   assert.equal(tweetAccounts.length, 2);
-  //   assert.ok(tweetAccounts.every(tweetAccount => 
-  //     tweetAccount.account.topic === "veganism"
-  //   ));
-  // });
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[0]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+
+    await assert.rejects(
+      program.methods.updateTweet("not-vegan", "Got milk?")
+        .accounts({
+          author: otherUser.publicKey,
+          tweet: tweetPDA,
+        })
+        .signers([otherUser])
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "A seeds constraint was violated");
+        return true;
+      },
+      "The author cannot update a different author's tweet.",
+    );
+  });
+
+  it("can not update to a topic with more than 50 characters", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[0]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+    const topicWith51Chars = 'x'.repeat(51);
+
+    await assert.rejects(
+      program.methods.updateTweet(topicWith51Chars, "Yay! Tofu!")
+        .accounts({
+          tweet: tweetPDA,
+          author: authorPublicKey,
+        })
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "The provided topic should be 50 characters long maximum.");
+        return true;
+      },
+      "The instruction should have failed with a 51-character topic.",
+    );
+  });
+
+  it("can not update to content with more than 280 characters", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[0]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+    const contentWith51Chars = 'x'.repeat(281);
+
+    await assert.rejects(
+      program.methods.updateTweet("veganism", contentWith51Chars)
+        .accounts({
+          tweet: tweetPDA,
+          author: program.provider.publicKey,
+        })
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "The provided content should be 280 characters long maximum.");
+        return true;
+      },
+      "The instruction should have failed with a 281-character content.",
+    );
+  });
+
+  it("can fetch all tweets", async () => {
+    const tweetAccounts = await program.account.tweet.all();
+    assert.equal(tweetAccounts.length, 3);
+  });
+
+  it("can filter tweets by author", async () => {
+    const authorPublicKey = program.provider.publicKey;
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset: 8, //Discriminator offset,
+          bytes: authorPublicKey.toBase58(),
+        }
+      }
+    ]);
+
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(tweetAccounts.every(tweetAccount => 
+      tweetAccount.account.author.toBase58() === authorPublicKey.toBase58()
+    ));
+  });
+
+  it("can filter tweets by topics", async () => {
+    const tweetAccounts = await program.account.tweet.all([
+      {
+        memcmp: {
+          offset: 8 + 32 + 8 + 8 + 4, // Discriminator + Author + Timestamp + Topic Length
+          bytes: bs58.encode(Buffer.from("veganism")),
+        }
+      }
+    ]);
+
+    assert.equal(tweetAccounts.length, 2);
+    assert.ok(tweetAccounts.every(tweetAccount => 
+      tweetAccount.account.topic === "veganism"
+    ));
+  });
+
+  it("can delete a tweet", async () => {
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[1]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+
+    await program.methods.deleteTweet()
+      .accounts({
+        author: authorPublicKey,
+        tweet: tweetPDA,
+      })
+      .rpc();
+
+    const tweetAccount = await program.account.tweet.fetchNullable(tweetPDA);
+    assert.equal(tweetAccount, null);
+  });
+
+  it("can not delete the tweet of a different user", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    await airdropToAccount(otherUser.publicKey, 1000000000);
+
+    const uniqueSeed = new anchor.BN(uuidParse(uuids[0]));
+    const authorPublicKey = program.provider.publicKey;
+    const [tweetPDA,] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("tweet"), authorPublicKey.toBuffer(), uniqueSeed.toArrayLike(Buffer)], 
+      program.programId,
+    )
+
+    await assert.rejects(
+      program.methods.deleteTweet()
+        .accounts({
+          author: otherUser.publicKey,
+          tweet: tweetPDA,
+        })
+        .signers([otherUser])
+        .rpc(),
+      (err: AnchorError) => {
+        assert.equal(err.error.errorMessage, "A seeds constraint was violated");
+        return true;
+      },
+      "The author cannot delete a different author's tweet.",
+    );
+  });
 });
